bfc35a97d721a22d1ca646109737817f0050b171
Won Maung Thein <wonmaungthein@gmail.com>
2018-08-19 14:23:41 +0100

    added Header, app component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,66 @@
 import React from "react";
 import "./Header.css";
+import Logo from "../../components/logo.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const logoStyle = {
+  height: 100
+};
 
 const Header = () => {
   return (
     <header className="app-header">
-      <h1 className="app-title">Welcome to Ubuntu</h1>
+      <Router>
+        <div>
+          <div>
+            <img src={Logo} style={logoStyle} alt="logo" />
+          </div>
+          <ul>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contactUs">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="/shelters">Shelters</Link>
+              </li>
+              <li>
+                <Link to="/referrals">Referrals</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+              <li>
+                <Link to="/addresses">Addresses</Link>
+              </li>
+            </ul>
+            {/*         
+        <li>ABOUT</li>
+        <li>MAKE A FEFERRAL</li>
+        <li>DONATIONS</li>
+        <li>VOLUNTEERING</li>
+        <li>CONTACT</li>
+        <li>OTHER ORGANIZATIONS</li> */}
+          </ul>
+          <div className="socialLogo">
+            <span>
+              <img src="#" alt="facebook logo" />
+              <img src="#" alt="twitter logo" />
+              <i class="fab fa-twitter fa-lg" />
+              <i class="fab fa-facebook fa-lg" />
+
+              {/* <i class="fas fa-stroopwafel fa-lg" />
+              <i class="fas fa-stroopwafel fa-3x" /> */}
+            </span>
+          </div>
+        </div>
+      </Router>
     </header>
   );
 };
--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,52 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "../Home/Home";
-import About from "../About/About";
-import ContactUs from "../ContactUs/ContactUs";
+
 import Header from "../../components/Header/Header";
-import Addresses from "../Addresses/Addresses";
-import Shelters from "../Shelters/Shelters";
-import Referrals from "../Referrals/Referrals";
-import Users from "../Users/Users";
+import AppBody from "../../components/Appbody/AppBody";
+import Footer from "../../components/Appbody/Footer/Footer";
 
 const App = () => (
-  <Router>
-    <div className="app">
-      <Header />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contactUs">Contact Us</Link>
-        </li>
-        <li>
-          <Link to="/shelters">Shelters</Link>
-        </li>
-        <li>
-          <Link to="/referrals">Referrals</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-        <li>
-        <Link to="/addresses">Addresses</Link>
-        </li>
-      </ul>
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contactUs" component={ContactUs} />
-      <Route path="/shelters" component={Shelters} />
-      <Route path="/referrals" component={Referrals} />
-      <Route path="/addresses" component={Addresses} />
-      <Route path="/users" component={Users} />
-    </div>
-  </Router>
+  <div className="app">
+    <Header />
+    <AppBody />
+    <Footer />
+  </div>
 );
 
 export default App;
